c3b0a9cbd19684092f12cb3d7e74c2e3cecd1436
frumentus <fab.schul2@outlook.de>
2025-12-19 01:01:20 +0100

    Refactor UpdatableEdgeExample, CustomEdge, and CustomNode components to enhance edge connection handling and streamline node rendering

--- examples/vite/src/UpdatableEdge/UpdatableEdgeExample.vue
+++ examples/vite/src/UpdatableEdge/UpdatableEdgeExample.vue
@@ -1,62 +1,78 @@
-<script lang="ts" setup>
-import type { Elements, FlowEvents, VueFlowStore } from '@vue-flow/core'
-import { ConnectionMode, VueFlow, useVueFlow } from '@vue-flow/core'
-import { Controls } from '@vue-flow/controls'
+<script setup>
+import { ref } from 'vue'
+import { Background } from '@vue-flow/background'
+import { VueFlow, useVueFlow } from '@vue-flow/core'
+import CustomEdge from './CustomEdge.vue'
+import CustomNode from './CustomNode.vue'
+const { updateEdge, addEdges } = useVueFlow()
 
-import '@vue-flow/controls/dist/style.css'
-
-const initialElements: Elements = [
+const nodes = ref([
   {
     id: '1',
     type: 'input',
-    label: 'Node <strong>A</strong>',
+    data: { label: 'Node <strong>A</strong>' },
     position: { x: 250, y: 0 },
   },
   {
     id: '2',
-    label: 'Node <strong>B</strong>',
+    type: 'custom',
+    data: { label: 'Node <strong>B</strong>' },
     position: { x: 100, y: 100 },
   },
   {
     id: '3',
-    label: 'Node <strong>C</strong>',
+    data: { label: 'Node <strong>C</strong>' },
     position: { x: 400, y: 100 },
     style: { background: '#D6D5E6', color: '#333', border: '1px solid #222138', width: 180 },
   },
-  { id: 'e1-2', source: '1', target: '2', label: 'Updatable target', updatable: 'target' },
-]
+])
 
-const { updateEdge } = useVueFlow()
+const edges = ref([
+  { 
+  id: 'e1-2', 
+  source: '1', 
+  target: '2', 
+  type: 'custom',
+  label: 'Updateable edge', 
+  updatable: true },
+])
 
-const elements = ref(initialElements)
-
-function onLoad(flowInstance: VueFlowStore) {
-  return flowInstance.fitView()
+function onEdgeUpdateStart(edge) {
+  console.log('start update', edge)
 }
 
-function onEdgeUpdateStart({ edge }: FlowEvents['edgeUpdateStart']) {
-  return console.log('start update', edge)
+function onEdgeUpdateEnd(edge) {
+  console.log('end update', edge)
 }
 
-function onEdgeUpdateEnd({ edge }: FlowEvents['edgeUpdateEnd']) {
-  return console.log('end update', edge)
+function onEdgeUpdate({ edge, connection }) {
+  updateEdge(edge, connection)
 }
 
-function onEdgeUpdate({ edge, connection }: FlowEvents['edgeUpdate']) {
-  return updateEdge(edge, connection)
+function onConnect(params) {
+  addEdges([params])
 }
 </script>
 
 <template>
   <VueFlow
-    v-model="elements"
-    :snap-to-grid="true"
-    :connection-mode="ConnectionMode.Loose"
-    @pane-ready="onLoad"
+    :nodes="nodes"
+    :edges="edges"
+    fit-view-on-init
+    :nodes-connectable="true"
+    :keepEdgeTypeDuringUpdate="true"
     @edge-update="onEdgeUpdate"
+    @connect="onConnect"
     @edge-update-start="onEdgeUpdateStart"
     @edge-update-end="onEdgeUpdateEnd"
   >
-    <Controls />
+    <Background />
+
+    <template #edge-custom="customEdgeProps">
+      <CustomEdge v-bind="customEdgeProps" />
+    </template>
+    <template #node-custom="customNodeProps">
+      <CustomNode v-bind="customNodeProps" />
+    </template>
   </VueFlow>
 </template>
